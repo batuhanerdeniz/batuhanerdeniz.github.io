95e9d5f44bd818e5f2652865478f111fdf50d126
Batuhan Erdeniz <44765515+batuhanerdeniz@users.noreply.github.com>
2023-09-23 11:37:02 +0300

    Add files via upload

---
+++ projects/melodies-for-autism/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon');
+    const menu = document.querySelector('.menu');
+
+    menuIcon.addEventListener('click', function () {
+        menu.classList.toggle('active');
+    });
+});
